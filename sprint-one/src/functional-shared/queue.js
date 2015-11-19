68faca643994ec01b4ea0ba722806eadabab6877
kat09kat09 <kat09kat09@gmail.com>
2015-11-19 15:05:28 -0500

    passed most tests for functional-shared queue (EXCEPT FOR "doesn't refer to this keyword"). paired w/ John

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,54 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  
+  //someinstance is this: 
+  var someInstance= {
+    enqueue: queueMethods.enqueue,
+    dequeue: queueMethods.dequeue,
+    size: queueMethods.size,
+    storage: {}
+  }; 
+
+  return someInstance; 
 };
 
-var queueMethods = {};
+var queueMethods= {
+  enqueue: function(value) {   
+    var currLoc= this.size(); 
+    this.storage[currLoc]= value; 
+    console.log('my size after enqueue', this.size()); 
+
+  },
+  dequeue: function () {
+    //remove the item at key 0
+    var item = this.storage[0];
+    delete this.storage[0]; 
+    var end= this.size();
+
+    // shift every key that's left over by 1
+    for (var key in this.storage) {
+      this.storage[parseInt(key,10)-1] = this.storage[key];   
+    }
+    
+    //delete the extra last key
+    delete this.storage[end]; 
+
+    console.log('my size after deque', this.size()); 
+    //return the removed value
+    return item; 
 
 
+  },
+  size: function() {
+    var count=0; 
+    for(var key in this.storage) {
+      count++; 
+    }
+    return count; 
+  }
+};
+
+ 
+// var newQueue= new Queue; // someinstance obj  {enqueue, dequueu, size}
+// newQueue.size(); // this: newQueue
