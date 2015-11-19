43a6d13a9b764d23f93a8300f509730a701b37f2
kat09kat09 <kat09kat09@gmail.com>
2015-11-19 15:04:18 -0500

    passed tests for functional- stack. Paired w/ John

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,29 @@
 var Stack = function(){
-  var someInstance = {};
+  var someInstance ={};
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[someInstance.size()] = value;
   };
 
   someInstance.pop = function(){
+    var item = storage[someInstance.size() - 1];
+    delete storage[someInstance.size() - 1];
+    return item; 
   };
 
   someInstance.size = function(){
+    var count=0; 
+    for(var key in storage) {
+      count++; 
+    }
+    return count; 
   };
 
   return someInstance;
 };
+
+// delete object['property']
