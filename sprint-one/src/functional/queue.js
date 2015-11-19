210ac5c3eae56878841274666de101371d2f1927
kat09kat09 <kat09kat09@gmail.com>
2015-11-19 15:04:56 -0500

    passed tests for functional-queue. paired w/ John

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,15 +4,39 @@ var Queue = function(){
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var currentCount=0; 
+
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[currentCount] = value;
+    currentCount++;
   };
 
   someInstance.dequeue = function(){
+    if(currentCount!==0) {
+      //remove the item at key 0
+      var item= storage[0]; 
+      delete storage[0]; 
+      // shift everyone else down by 1
+      for(var key in storage) {
+        storage[parseInt(key,10)-1] = storage[key]; 
+      }
+      //delete the key with the highest # (currentCount)
+      delete storage[currentCount]; 
+
+      //decrementing count
+      currentCount--; 
+
+      //return item we removed
+      return item;
+
+    }
+    
   };
 
   someInstance.size = function(){
+    return currentCount; 
   };
 
   return someInstance;
