8aa35d89d54b6ff6bd29de3a28bfb32a15a430ce
kat09kat09 <kat09kat09@gmail.com>
2015-11-20 15:19:00 -0500

    passed tests for linked list! paired with john

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,47 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var currentNode= Node(value); 
+    if(list.head===null && list.tail===null) {
+      list.head= currentNode; 
+      list.tail= currentNode; 
+    // } else if (list.tail===null) {
+    //   list.tail= currentNode;
+    } else {
+      list.tail.next= currentNode; 
+      list.tail = currentNode; 
+    }
+    console.log('list', list); 
   };
 
   list.removeHead = function(){
+    var originalHead; 
+    if(!list.head) {
+      return; 
+    } else if (list.head && !list.head.next){
+      originalHead= list.head.value; 
+      list.head= null; 
+      list.tail= null; 
+    } else {
+      originalHead= list.head.value; 
+      list.head= list.head.next;
+      
+      
+    }
+    return originalHead; 
   };
 
   list.contains = function(target){
+    var found = false; 
+    var currentNode= list.head; 
+    while (!found && currentNode) {
+      if(currentNode.value===target) {
+        found= true; 
+      }
+      currentNode= currentNode.next; 
+    }
+
+    return found; 
   };
 
   return list;
