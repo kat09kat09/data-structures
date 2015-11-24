b563aa1d78d34b62b5c74230daf17fb13317d994
kat09kat09 <kat09kat09@gmail.com>
2015-11-23 19:28:38 -0500

    added tests for doubly linked list

---
+++ sprint-two/spec/doublyLinkedListSpec.js
@@ -0,0 +1,79 @@
+describe('doublylinkedList', function() {
+  var linkedList;
+
+  beforeEach(function() {
+    linkedList = DoublyLinkedList();
+  });
+
+  it('should have a head and tail', function() {
+    expect(linkedList).to.have.property("head");
+    expect(linkedList).to.have.property("tail");
+  });
+
+  it('should have methods named "addToTail", "removeHead", "contains", "addToHead", and "removeTail"', function() {
+    expect(linkedList.addToTail).to.be.a("function");
+    expect(linkedList.removeHead).to.be.a("function");
+    expect(linkedList.contains).to.be.a("function");
+    expect(linkedList.addToHead).to.be.a("function");
+    expect(linkedList.removeTail).to.be.a("function");
+  });
+
+  it('should designate a new tail when new nodes are added', function(){
+    linkedList.addToTail(4);
+    expect(linkedList.tail.value).to.equal(4);
+    linkedList.addToTail(5);
+    expect(linkedList.tail.value).to.equal(5);
+  });
+
+  it('should remove the head from the list when removeHead is called', function(){
+    linkedList.addToTail(4);
+    linkedList.addToTail(5);
+    expect(linkedList.head.value).to.equal(4);
+    linkedList.removeHead();
+    expect(linkedList.head.value).to.equal(5);
+  });
+
+  it("should return the value of the former head when removeHead is called", function(){
+    linkedList.addToTail(4);
+    expect(linkedList.removeHead()).to.equal(4);
+  });
+
+  it("should contain a value that was added", function(){
+    linkedList.addToTail(4);
+    linkedList.addToTail(5);
+    expect(linkedList.contains(4)).to.equal(true);
+    expect(linkedList.contains(5)).to.equal(true);
+    expect(linkedList.contains(6)).to.equal(false);
+  });
+
+  it('should not contain a value that was removed', function(){
+    linkedList.addToTail(4);
+    linkedList.addToTail(5);
+    linkedList.removeHead();
+    expect(linkedList.contains(4)).to.equal(false);
+  });
+
+  it('should designate a new head when new nodes are added to the head', function(){
+    linkedList.addToHead(4);
+    expect(linkedList.head.value).to.equal(4);
+    linkedList.addToHead(5);
+    expect(linkedList.head.value).to.equal(5);
+  });
+
+  it('should have the old head pointing to the new head as the previous link', function(){
+    linkedList.addToHead(4);
+    linkedList.addToHead(5);
+    expect(linkedList.head.next.previous.value).to.equal(5); 
+  });
+
+
+  it("should return the value of the former tail when removeTail is called", function(){
+    linkedList.addToTail(4);
+    linkedList.addToTail(5);
+    expect(linkedList.removeTail()).to.equal(5);
+  });
+
+  
+
+  // add more tests here to test the functionality of linkedList
+});
