70a29d10890de501afd528e8a1eecc90c61d6254
kat09kat09 <kat09kat09@gmail.com>
2015-11-24 13:49:42 -0500

    implemented: sets should handle objects of any type

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,23 +1,47 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = {};
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
-  this._storage[item]= undefined; 
+	var exist= this._storage.some(function(storedItem){
+		return _.isEqual(storedItem, item)
+	}); 
+
+	if(!exist) {
+		this._storage.push(item); 
+	}
+
 };
 
 setPrototype.contains = function(item){
-  return item in this._storage; 
+	//need to implement deep checking for objects/arrays/functions/ etc
+	return this._storage.some(function(storedItem) {
+		return _.isEqual(storedItem, item); 
+	}); 
+	
 };
 
 setPrototype.remove = function(item){
-  delete this._storage[item]; 
+	var idx=  this._storage.reduce(function(targetIdx,storedItem ,idx) {
+		if(_.isEqual(storedItem, item)) {
+			targetIdx= idx; 
+		} 
+		return targetIdx; 
+	}, -1); 
+
+	if(idx>=0) {
+		this._storage.splice(idx,1); 
+	}
+	
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ add: o(1)
+ contains: o(n)
+ remove: o(1)
  */
