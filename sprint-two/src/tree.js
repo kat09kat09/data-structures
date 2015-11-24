f9c4ca1d40bf31817818b1be6391701b5f1d9b9c
kat09kat09 <kat09kat09@gmail.com>
2015-11-23 20:25:26 -0500

    implemented: added parent property , removeFromParent method

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,11 @@
-//hello hi
+
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
+  newTree.parent= null; 
   newTree.addChild = treeMethods.addChild;
   newTree.contains= treeMethods.contains; 
+  newTree.removeFromParent= treeMethods.removeFromParent; 
   
   // your code here
   newTree.children = [];  // fix me
@@ -11,14 +13,11 @@ var Tree = function(value){
   return newTree;
 };
 
-
-
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
   var subTree= Tree(value); 
+  subTree.parent= this; 
   this.children.push(subTree); 
 };
 
@@ -34,6 +33,40 @@ treeMethods.contains = function(target){
   }
   return found; 
 };
+
+treeMethods.removeFromParent= function (target) {
+  var children;
+  var idx; 
+  var result; 
+  //returns the disassociated tree
+  if(this.value=== target) {
+    //find the target value in the parent's children & disassocate it
+    var children= this.parent.children;
+    var idx= children.reduce(function(targetIdx, child, idx) {
+      if(child.value===target) {
+        targetIdx= idx; 
+      } else {
+        targetIdx= targetIdx; 
+      }
+      return targetIdx; 
+    }); 
+
+    children.splice(idx,1); 
+
+    //disassociate the current subtree from it's parent 
+    this.parent= null; 
+
+    return this; 
+  } else {
+    //keep recursing
+    for(var i= 0; i< this.children.length; i++) {
+      result= this.children[i].removeFromParent(target); 
+
+      //if you've found the target on this child's subtree, you're done
+      if(result) { return result; }
+    }; 
+  }
+}
 
 
 
